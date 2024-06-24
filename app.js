9e38a1469644ced71373dac28f4442a121861f23
z3san <ahtisamhaque@gmail.com>
2024-06-24 19:31:37 +0600

    nested if else

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-
+let name = "rahim"
+let age = "24"
+let number = 30
+if(number==30){
+if(typeof number == "string"){
+    console.log("all ok");
+}
+else{
+    console.log("Number is 30 but not string");
+}
+}
+else{
+    console.log("Kichui naa, Wrong");
+}
