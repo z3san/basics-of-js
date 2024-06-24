10e39f6df6ae7405a7c1dd35630f7d9a3d5cdb0d
z3san <ahtisamhaque@gmail.com>
2024-06-24 19:42:08 +0600

    even odd

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-let name = "rahim"
-let age = "24"
-let number = 30
-if(number==30){
-if(typeof number == "string"){
-    console.log("all ok");
-}
-else{
-    console.log("Number is 30 but not string");
-}
-}
-else{
-    console.log("Kichui naa, Wrong");
-}
+
+// let number = 30
+// if(number==30){
+// if(typeof number == "string"){
+//     console.log("all ok");
+// }
+// else{
+//     console.log("Number is 30 but not string");
+// }
+// }
+// else{
+//     console.log("Kichui naa, Wrong");
+// }
  
+
+// let num = 103;
+// if ( num % 2 != 0 ){
+// console.log("This is a odd number");
+// }
+// else{
+//     console.log("This is a even number");
+// }
